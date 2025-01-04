11c79f9021e904ae6c5210d662b01544895948b5
edricbarnwal <social24.05barnwal.ec@gmail.com>
2025-01-04 22:33:00 +0530

    Update TO DO List.py

--- TO DO List.py
+++ TO DO List.py
@@ -2,31 +2,52 @@ TASK_DATA = []
 
 def add_task(task):
     TASK_DATA.append(task)
+    print (f"'{task}'' Task added Successfully\n")
 
+def remove_task(task):
+    TASK_DATA.remove(task)
+    print (f"'{task}'' Task removed Successfully\n")
+
+def show_task(task):
+    for i, item in enumerate(task, start=1):
+        print (f"{i} - {item}")
+    print ("\n")
+    
 print ("*** TODO List ***")
 print ("""Enter '1' to Add Task
 Enter '2' to Remove Task
 Enter '3' to Show Task
-Enter '4' to Edit Task
-Enter '5' to Exit
+Enter '4' to Exit
 """)
 
-while True:
-    user_input = input("Enter input: ")
-    if user_input == '1':
-        Add_Task = input("Enter task to Add: ")
-        add_task(Add_Task)
-        print (f"'{Add_Task}'' Task added Successfully")
+def TODO_Function():
+    while True:
+        user_input = input("Enter input: ")
         
-    elif user_input == '2':
-        if not TASK_DATA:
-            print ("First Add a Task")
+        if user_input == '1':
+            Add_Task = input("Enter task to Add: ")
+            add_task(Add_Task)
+            
+        elif user_input == '2':
+            if not TASK_DATA:
+                print ("No task. Enter '1' to Add Task.\n")
+            else:
+                show_task(TASK_DATA)
+                Remove_Task = int(input("Enter the number of the task to remove: "))
+                remove_task(TASK_DATA[Remove_Task-1])
+    
+        elif user_input == '3':
+            if not TASK_DATA:
+                print ("No task. Enter '1' to Add Task.\n")
+            else:
+                show_task(TASK_DATA)
+        
+        elif user_input == '4':
+            print ("Exiting TODO List. Thank You for Using")
+            break
+                
         else:
-            print (TASK_DATA)
-            Remove_Task = input("Enter a Task To Remove")
-    elif user_input == '3':
-        print ("Show Task")
-    elif user_input == '4':
-        print ("Edit Task")
-    else:
-        print ("Enter a valid input.")
+            print ("Enter a valid input.")
+
+if __name__ == "__main__":
+    TODO_Function()
