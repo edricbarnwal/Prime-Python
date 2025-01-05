84ab5c4c198f729baefb88f14aff95fd91829eec
edricbarnwal <social24.05barnwal.ec@gmail.com>
2025-01-05 12:02:29 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -5,7 +5,17 @@ ALPHABET =  (string.ascii_letters)
 NUMBER = (string.digits)
 SPECIAL_CHARACTER = (string.punctuation)
 
-User_Password_len = int(input("How many characters do you want in a password: ))
+User_Password_len = int(input("How many characters do you want in a password: "))
 Special_Character = input("Do you need a special character: ")
 
 def generate_password(number):
+    New_List = ALPHABET + NUMBER
+    random_num = []
+    for x in range(number):
+        newl = random.choice(New_List)
+        random_num.append(newl)
+        
+    print (random_num)
+        
+        
+generate_password(User_Password_len)       
