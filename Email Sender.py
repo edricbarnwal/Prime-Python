309179e79c0ba40b2490a055b63c83e860e9b511
edricbarnwal <social24.05barnwal.ec@gmail.com>
2025-01-11 07:30:49 +0530

    Update Email Sender.py

--- Email Sender.py
+++ Email Sender.py
@@ -1,23 +1,26 @@
 import smtplib
 
-sender_email = "xxxx@gmail.com"
-password = "xxxabc"
+while True:
+    sender_email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    receiver_email = input("Enter the recipient's email address: ")
+    subject_input = input("What is the subject of your email? ")
+    body_input = input("What is the message? ")
 
-receiver_mail = "xyz123@gail.com"
+    subject = f"Subject: {subject_input}\n"
+    try:
+        sm = smtplib.SMTP("smtp.gmail.com", 587)
+        sm.starttls()
+        sm.login(sender_email, password)
+        message = subject + body_input
+        sm.sendmail(sender_email, receiver_email, message)
+        print ("Email Sent Succesfully!")
+    except Exception as e:
+        print (f"An Error Occured: {e}")
+    finally:
+        sm.quit()
 
-try:
-    sm = smtplib.SMTP("smtp.gmail.com", 587)
-    sm.starttls()
-    sm.login(sender_email, password)
-    
-    subject = "Subject: Test Email\n"
-    body = "Enter your message you want to send"
-    message = subject + body
-    sm.sendmail(sender_email, receiver_mail, message)
-    print ("Email Sent Succesfully!")
-
-except Exception as e:
-    print (f"An Error Occured: {e}")
-
-finally:
-    sm.quit()
+    send_again = input("Do you want to send again (y/n)?: ")
+    if send_again != 'y':
+        print ("Thanks for using")
+        break
